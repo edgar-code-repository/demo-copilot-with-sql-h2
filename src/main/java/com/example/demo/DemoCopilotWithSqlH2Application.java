0bfac1383b3daca265b8a7fe4ab75ae6a429b541
Edgardo <ecofre@gmail.com>
2026-06-09 22:28:22 -0400

    Initial structure of the project

---
+++ src/main/java/com/example/demo/DemoCopilotWithSqlH2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCopilotWithSqlH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCopilotWithSqlH2Application.class, args);
+	}
+
+}
